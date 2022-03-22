3a1316c7b97500ef22758c559931c69ee2e877cf
anusoda <35657704+anusoda@users.noreply.github.com>
2022-03-22 08:59:23 -0400

    mkmk

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("deez nuts");
-        System.out.println("adsfasdf");
+        System.out.println("rink");
     }
 }
