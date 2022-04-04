a5c9489e6f9cd9d06f461765524c55ac221fbad5
anusoda <35657704+anusoda@users.noreply.github.com>
2022-04-04 08:57:53 -0400

    .

--- src/sf/Arena.java
+++ src/sf/Arena.java
@@ -1,4 +1,7 @@
 package sf;
+import java.awt.*;
+import javax.swing.*;
+
 import utility.*;
 public class Arena {
     private int playerWins, opponentWins;
@@ -28,5 +31,22 @@ public class Arena {
     }
     public void frameUpdate(boolean[] playerInputs, boolean[] opponentInputs) { // left, right, light, heavy, special
         
+    }
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setPreferredSize(new Dimension(1080, 748));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(Color.BLACK);
+        mainPanel.setBounds(0, 0, 1080, 720);
+        mainPanel.setLayout(null);
+        frame.add(mainPanel);
+        JPanel gamePanel = new JPanel();
+        gamePanel.setLayout(null);
+        gamePanel.setBounds(0, 40, 1080, 640);
+        mainPanel.add(gamePanel);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
